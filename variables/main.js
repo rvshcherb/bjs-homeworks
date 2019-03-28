d2737ea806dd564e90fa32c53bc1b499b79d7edd
rvshcherb <shcherbinin_rv@mail.ru>
2019-03-28 14:22:29 +0300

    initial commit

---
+++ variables/main.js
@@ -0,0 +1,24 @@
+// Задача №1
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+
+console.log(result);
+
+
+// Задача №2
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+
+let average = (algebra + geography + physics) / 3;
+
+console.log(average);
+
+// Задача №3
+let name = 'Роман';
+let message = `Привет, мир! Меня зовут ${name}`;
+
+console.log(message);
