8bc7aa266044cc4593cd175ad6d06f23cb03bbdb
rvshcherb <shcherbinin_rv@mail.ru>
2019-04-11 12:58:11 +0300

    first commit functions second commit

--- functions/main.js
+++ functions/main.js
@@ -1,29 +1,101 @@
+// Задача №1
 function getSolutions(a,b,c) {
   let D = Math.pow(b,2) - 4 * a * c;
-  let result = new Object;
   let roots = [];
 
   if (D < 0) {
-    result.D = D;
+    return {D}
   } else if (D == 0) {
     let x1 = -b / 2 * a;
-    result.D = D;
     roots.push(x1);
-    result.roots = roots;
-    console.log(roots);
+    return {D,roots};
   } else {
     let x1 = (-b + Math.sqrt(D)) / (2 * a);
     let x2 = (-b - Math.sqrt(D)) / (2 * a);
-    result.D = D;
     roots.push(x1,x2);
-    result.roots = roots;
-    console.log(roots);
+    return {D,roots};
   }
-  return result;
 }
 
-console.log(getSolutions(1,-4,-5));
-
-function showSolutionMessge(a,b,c) {
-  console.log(`вычисляем корни квадратного уравнения ${a}x2 + ${b}x + ${c}`)
+function showSolutionsMessage(a,b,c) {
+  let result = getSolutions(a,b,c);
+  console.log(`Вычисляем корни квадратного уравнения ${a}x² + (${b}x) + (${c})`);
+  
+  if (result.D < 0) {
+    console.log(`Значение дискриминанта: ${result.D}\nУравнение не имеет вещественных корней`);
+  } else if (result.D === 0) {
+    console.log(`Значение дискриминанта: ${result.D}\nУравнение имеет один корень X₁: ${result.roots}`);
+  } else {
+    console.log(`Значение дискриминанта: ${result.D}\nУравнение имеет два корня корень X₁: ${result.roots[0]}, X₂: ${result.roots[1]}`);
+  }
 }
+
+showSolutionsMessage(3,-4,2);
+showSolutionsMessage(1,-6,9);
+showSolutionsMessage(1,-4,-5);
+
+// Задача №2
+function getPersonalData(secretData) {
+  function chooseFirstName() {
+    if(secretData.aaa === 0) {
+      return 'Эмилио';
+    } else {
+      return 'Родриго';
+    }
+  }
+
+  function chooseLastName() {
+    if(secretData.bbb === 0) {
+      return 'Эмилио';
+    } else {
+      return 'Родриго';
+    }
+  }
+
+  let name = {
+    firstName: chooseFirstName(),
+    lastName: chooseLastName()
+  }
+  return name;
+}
+
+console.log(getPersonalData({aaa: 0, bbb:0}));
+console.log(getPersonalData({aaa: 0, bbb:1}));
+console.log(getPersonalData({aaa: 1, bbb:0}));
+console.log(getPersonalData({aaa: 1, bbb:1}));
+
+//Задача №3
+function getAverageScore(data) {
+  for (let prop in data) {
+    data[prop] = averageSubject();
+
+    function averageSubject(){
+      let sum = 0;
+      for (let i = 0; i < data[prop].length; i++) {
+        sum += data[prop][i];
+      }
+      return sum / data[prop].length;;
+    }
+  }
+
+  data.average = averageTotal();
+
+  function averageTotal() {
+    let quant = 0;
+    let sum = 0;
+    for (let prop in data) {
+      sum += data[prop];
+      quant++;
+    }
+    return sum / quant;
+  }
+  return data;
+}
+
+console.log(getAverageScore({
+  algebra: [2,4,2,4,4,2,1,2,4,2,2,3],
+  geomtry: [3,5,5,3,3,5,5,3,5,3,2,5,3,3,5,4,5,5,5],
+  russian: [2,2,2,2,2,2,3,5,3,2,1,2],
+  music: [3,5,4,5,5,5],
+  physic: [3,5,5,3,3,5,4,5,5,5]
+}));
